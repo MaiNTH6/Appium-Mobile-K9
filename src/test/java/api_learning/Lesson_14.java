4dfa9a7883f20bb9f97c30e2353867825e66128b
vuvandao <daovv@sskpi.com>
2022-07-07 14:43:37 +0700

    lesson 14

--- src/test/java/api_learning/Lesson_14.java
+++ src/test/java/api_learning/Lesson_14.java
@@ -1,2 +1,43 @@
-package api_learning;public class Lesson_14 {
+package api_learning;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class Lesson_14 {
+
+    public static void main(String[] args) {
+
+        //Send a request inti Appium Server -- lunch the app
+
+        AppiumDriver<MobileElement> appiumDriver = null;
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("automationName", "uiautomator2");
+        desiredCapabilities.setCapability("udid", "emulator-5554");
+        desiredCapabilities.setCapability("appPackage", "com.wdiodemoapp");
+        desiredCapabilities.setCapability("appActivity", "com.wdiodemoapp.MainActivity");
+
+        // Init appium session
+
+        try {
+            URL appiumServer = new URL("http://localhost:4723/wd/hub");
+            appiumDriver = new AndroidDriver<MobileElement>(appiumServer, desiredCapabilities);
+
+            // DEBUG PURPOSE ONLY
+            Thread.sleep(3000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        if(appiumDriver !=null)
+            appiumDriver.quit();
+
+    }
+
+
+
 }
