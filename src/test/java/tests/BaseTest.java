130b4d439203916ce95284a2687c9c303992432f
Your Name <you@example.com>
2022-08-02 23:54:21 +0700

    lesson 21

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import driver.DriverFactory;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import plaform.Platform;
+
+public class BaseTest {
+
+    protected static AppiumDriver<MobileElement> appiumDriver;
+
+    @BeforeTest
+    public void initAppiumSession(){
+        appiumDriver = DriverFactory.getDriver(Platform.ANDROID);
+
+    }
+
+    @AfterTest(alwaysRun = true)
+    public void  quitAppiumSession(){
+        appiumDriver.quit();
+
+    }
+}
--- src/test/java/tests/authen/LoginTestWithBaseTest.java
+++ src/test/java/tests/authen/LoginTestWithBaseTest.java
@@ -1,3 +1,4 @@
+
 package tests.authen;
 
 import driver.DriverFactory;
@@ -9,23 +10,18 @@ import plaform.Platform;
 import test_data.DataObjiectBuilder;
 import test_data.models.LoginCred;
 import test_flows.authentication.LoginFlow;
+import tests.BaseTest;
 
-public class LoginTestWithDataBuilder {
+public class LoginTestWithBaseTest extends BaseTest {
     @Test(dataProvider = "loginCredData")
-    public static void testLogin(LoginCred loginCred) {
-        AppiumDriver<MobileElement> appiumDriver = DriverFactory.getDriver(Platform.ANDROID);
+    public  void testLogin(LoginCred loginCred) {
 
-        try{
                 LoginFlow loginFlow = new LoginFlow(appiumDriver, loginCred.getEmail(), loginCred.getPassword());
                 loginFlow.gotoLoginScreen();
                 loginFlow.login();
                 loginFlow.verifyLogin();
 
-        }catch (Exception e){
-            e.printStackTrace();
         }
-        appiumDriver.quit();
-    }
 
     @DataProvider
 
